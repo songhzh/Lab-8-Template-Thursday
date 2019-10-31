299af57cda3c85c31a90640d72132b3366bfe259
Song Hui Zhang <songhui@ualberta.ca>
2019-10-31 17:20:42 -0600

    add setcityname test

--- app/src/main/java/com/example/simpleparadox/listycity/City.java
+++ app/src/main/java/com/example/simpleparadox/listycity/City.java
@@ -17,6 +17,10 @@ public class City implements Comparable<City>{
         return this.province;
     }
 
+    void setCityName(String name) {
+
+    }
+
     @Override
     public int compareTo(City o) {
         return city.compareTo(o.getCityName());
